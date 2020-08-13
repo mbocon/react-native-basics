2e9dbb4c9f11431b26faeebc36e8a1bc7c3cacc5
Michael Bocon <mikebocon@gmail.com>
2020-08-13 13:46:09 -1000

    updated some stuff

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -4,13 +4,13 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 const HomeScreen = ({ navigation }) => {
 	return (
 		<View>
-      <Text style={styles.text}>Hello Home screen!</Text>
-      <Button title='Go to components demo' onPress={() => navigation.navigate('Components')} />
-      <Button title='Go to list demo' onPress={()=> navigation.navigate('List')} />
-      <Button title='Go to image demo' onPress={()=> navigation.navigate('Image')} />
-      <Button title='Go to counter demo' onPress={()=> navigation.navigate('Counter')} />
-      <Button title='Go to colors demo' onPress={()=> navigation.navigate('Colors')} />
-      <Button title='Go to squareScreen demo' onPress={()=> navigation.navigate('Square')} />
+			<Text style={styles.text}>Hello Home screen!</Text>
+			<Button title='Go to components demo' onPress={() => navigation.navigate('Components')} />
+			<Button title='Go to list demo' onPress={() => navigation.navigate('List')} />
+			<Button title='Go to image demo' onPress={() => navigation.navigate('Image')} />
+			<Button title='Go to counter demo' onPress={() => navigation.navigate('Counter')} />
+			<Button title='Go to colors demo' onPress={() => navigation.navigate('Colors')} />
+			<Button title='Go to squareScreen demo' onPress={() => navigation.navigate('Square')} />
 		</View>
 	);
 };
